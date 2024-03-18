520d834dba3cbb9b06567f2da14864eff0e10fde
Rodrigo <ridrigocorreia@gmail.com>
2024-03-18 16:06:17 -0300

    final

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,9 +34,9 @@ function App() {
   }
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
 
-    // utilizar filter.
+    const updatedRepos = repos.filter(repo => repo.id !== id)
+    setRepos(updatedRepos)
   }
 
 
